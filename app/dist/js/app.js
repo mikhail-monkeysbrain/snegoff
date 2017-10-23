8bb156ba7d4edecca4153b1c22862f3d9b4edc37
Столповских Михаил <monkeysbrain@yandex.ru>
2017-10-23 12:36:27 +0500

    without catalog

--- app/dist/js/app.js
+++ app/dist/js/app.js
@@ -58,3 +58,53 @@ $(document).ready(function() {
   }, timeout);
 
 });
+
+$(document).ready(function(){
+  $(".slide-one").owlCarousel({
+    loop:true,
+    nav:true,
+    margin:10,
+    navText: [],
+    responsive:{
+      0:{
+        items:1
+      }
+    }
+  });
+
+  $(".slide-two").owlCarousel({
+    loop:true,
+    nav:true,
+    margin:10,
+    navText: [],
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:2
+      },
+      1000:{
+        items:4
+      }
+    }
+  });
+
+  $(".slide-three").owlCarousel({
+    loop:true,
+    nav:true,
+    margin:10,
+    navText: [],
+    responsive:{
+      0:{
+        items:2
+      },
+      600:{
+        items:5
+      },
+      1000:{
+        items:6
+      }
+    }
+  });
+});
